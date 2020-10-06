b306cda3b587dc335cf9fae615af8957ce1709e9
zAm-mi <sam.toolkt@gmail.com>
2020-10-06 08:54:54 +0000

    Added Security and Views

--- openacademy/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ openacademy/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -3,15 +3,18 @@
     'name': "openacademy",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Open Academy module for managing trainings:
+            - training courses
+            - training sessions
+            - attendees registration
+    """,
 
     'description': """
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Toolkit",
+    'website': "http://www.toolkt.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
@@ -24,9 +27,10 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'security/ir.model.access.csv',
+#         'views/views.xml',
+#         'views/templates.xml',
+        'views/openacademy.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -27,9 +27,10 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'security/ir.model.access.csv',
+#         'views/views.xml',
+#         'views/templates.xml',
+        'views/openacademy.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
